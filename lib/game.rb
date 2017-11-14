834143d60382422389b338ab47f968a138ed66bb
LJ Nissen <lj.nissen@gmail.com>
2017-11-14 18:26:54 +0100

    Fix vertical lines in gosu

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,10 @@
 require 'gosu'
 
 class Game < Gosu::Window
-	attr_accessor :width
+	attr_accessor :width, :cell_width
 	def initialize
 		@width = 600
+		@cell_width = @width/3
 		super(width, width, false)
 	end
 	def button_down(id)
@@ -11,8 +12,16 @@ class Game < Gosu::Window
 			when Gosu::KbQ then close
 		end
 	end
+	
 	def needs_cursor?
 		true
+	end
+
+	def draw
+		[cell_width, cell_width*2].each do |w|
+			draw_line(w, 0, Gosu::Color::WHITE,
+						w, width, Gosu::Color::WHITE)
+		end
 	end
 end
 
